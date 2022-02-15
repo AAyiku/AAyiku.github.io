8a73d834ec8594cab642ae8f411691ddaa89744c
fosua-labs <57545392+AAyiku@users.noreply.github.com>
2022-02-15 14:51:08 +0000

    Delete defines.php

--- defines.php
+++
@@ -1,5 +0,0 @@
-<?PHP
-define('INSTAGRAM_APP_ID', '450705353436886');
-define('INSTAGRAM_APP_SECRET', '3f20d72ae0d074a9406145dfe49b441c');
-define('INSTAGRAM_APP_REDIRECT_URI','https://aayiku.github.io/index.php')
-?>
